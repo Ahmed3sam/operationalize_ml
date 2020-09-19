ff4bde5e2b00531e99be9cda20679f93ca86b48a
Ubuntu <ubuntu@ip-172-31-46-107.us-east-2.compute.internal>
2020-09-19 07:09:41 +0000

    my comment

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ahmed3sam/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flasksklearn --image=$dockerpath --port=80 --labels app=flasksklearn
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward flasksklearn 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
-# Step 2:  
+dockerpath=ahmed3sam/api
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+export DOCKER_ID_USER="ahmed3sam"
+docker login
+docker tag api $DOCKER_ID_USER/api
+docker push $DOCKER_ID_USER/api
 
 # Step 3:
 # Push image to a docker repository
+docker push ahmed3sam/api
+
